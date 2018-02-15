4e885550954f3fe0b4596214e53c8a0214cc4016
NamanGarg <gargnaman0893@gmail.com>
2018-02-15 19:16:20 +0530

    First commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
       : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
-    displayText:speech,
+    displayText:"Naman",
     source: "webhook-echo-sample"
   });
 });
