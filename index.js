6e67522bb8af7b31f000250349e8a1f9a8814933
NamanGarg <gargnaman0893@gmail.com>
2018-02-21 16:00:59 +0530

    Yes

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ restService.post("/echo", function(req, res) {
 
 restService.get('/', function(req, res) {
 		res.jsonp({"message":config.salutation, "id":config.sessionid,"done":config.success});
-		config.salutation="";
+		//config.salutation="";
 });
 
 function completed(){
